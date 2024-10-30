67db3be1d18d1d57e7232348ee88e9f53a1b85dc
User <alezalez141@gmail.com>
2024-10-30 09:54:53 +0300

    1

--- 112.py
+++ 112.py
@@ -1,11 +1,18 @@
-epsilon = float(input('Введите точность (e):'))
-
-sum_series = 0.0
-n = 1 # Начально значение n
 while True:
-    t = 1 / (n**2) #Вычисляем n-ый член ряда
-    if abs(t) < epsilon: #Проверяем условие остановки
+    while True:
+        try: epsilon = float(input('Введите точность (e):'))
+        except ValueError:
+            print('Введены некорректные данные')
+        else: break
+    if epsilon <= 0:
+        print('Введено некорретное число')
+    else: break
+sum_series = 0
+n = 1
+while True:
+    t = 1 / (n**2)
+    if abs(t) < epsilon:
         break
-    sum_series += t #Добавляем член ряда к сумме
+    sum_series += t
     n += 1
 print ('Приблизительная сумма ряда' , sum_series)
